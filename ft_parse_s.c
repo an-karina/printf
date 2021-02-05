05c95fe3c544134b307ab0078a83c011c1e24667
an-karina <an.karina.kar@gmail.com>
2021-02-05 05:00:56 +0300

    wtf3

--- ft_parse_s.c
+++ ft_parse_s.c
@@ -21,11 +21,15 @@ void	ft_parse_s(char **format, va_list arguments, t_buffer *buf)
 	int am_symb;
 	
 	str = va_arg(arguments, char *);
-	if (str)
-		str_len = ft_strlen(str);
-	else
+	if (!str)
+	{
+		write(1, "(null)", 6);
+		buf->length+=6;
 		str_len = 0;
-		
+		return ;
+	}
+	else
+	str_len = ft_strlen(str);
 	am_zero = 0;
 	am_space = 0;
 	if ((buf->precision >= 0) && (buf->precision < str_len))
@@ -37,12 +41,6 @@ void	ft_parse_s(char **format, va_list arguments, t_buffer *buf)
 	buf->length += str_len + am_space + am_zero;
 	if (buf->minus)
 	{
-		if (!str)
-		{
-			write(1, "(null)", 6);
-			buf->length += 6;
-			am_space -= 6;
-		}
 		while (str_len--)
 			write(1, str++, 1);
 		while (am_space--)
@@ -50,17 +48,10 @@ void	ft_parse_s(char **format, va_list arguments, t_buffer *buf)
 	}
 	else
 	{
-		if (!str)
-			am_space -= 6;
 		while (am_space--)
 			write(1, " ", 1);
 		while (am_zero--)
 			write(1, "0", 1);
-		if (!str)
-		{
-			write(1, "(null)", 6);
-			buf->length += 6;
-		}
 		while (str_len--)
 			write(1, str++, 1);
 	}
